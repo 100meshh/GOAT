4d995e5aeac094108d405aa3e8fa21f5a0355830
Somesh Shukla <someshshuklacollege@gmail.com>
2025-07-27 19:45:42 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (77.34%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-int f(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-    if(j<0 || j>=matrix[0].size()) return 1e8;
-    if(i==0) return matrix[0][j];
-    if(dp[i][j]!=-1) return dp[i][j];
-    int up=matrix[i][j]+f(i-1,j,matrix,dp);
-    int upleft=matrix[i][j]+f(i-1,j-1,matrix,dp);
-    int upright=matrix[i][j]+f(i-1,j+1,matrix,dp);
-    return dp[i][j]= min(up,min(upleft,upright));
-}
+
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<int>prev(m,0);
         int mini=1e8;
-        for(int j=0;j<m;j++) dp[0][j]=matrix[0][j];
+        for(int j=0;j<m;j++) prev[j]=matrix[0][j];
         for(int i=1;i<n;i++){
+            vector<int>curr(m,0);
             for(int j=0;j<m;j++){
-                int up=matrix[i][j]+dp[i-1][j];
+                int up=matrix[i][j]+prev[j];
                 int upleft=matrix[i][j];
-      if(j-1>=0) upleft+=dp[i-1][j-1];
+      if(j-1>=0) upleft+=prev[j-1];
                 else upleft+=1e8;
 
                 int upright=matrix[i][j];
-      if(j+1<m) upright+=dp[i-1][j+1];
+      if(j+1<m) upright+=prev[j+1];
                 else upright+=1e8;
-                dp[i][j]=min(up,min(upleft,upright));
+                curr[j]=min(up,min(upleft,upright));
             }
+            prev=curr;
         }
         // int mini=1e8;
         for(int j=0;j<m;j++){
-            mini=min(mini,dp[n-1][j]);
+            mini=min(mini,prev[j]);
         }
         return mini;
     }
