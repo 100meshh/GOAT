c4f2d39034fcd3d1a87690e629d3ae78587e4ffa
Somesh Shukla <someshshuklacollege@gmail.com>
2025-09-05 23:52:16 +0530

    Time: 4 ms (42.65%) | Memory: 18 MB (27.85%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     void combi(vector<int>& candidates,int index,vector<vector<int>>&ans,vector<int>&temp,int target){
         if (target < 0) return;
+        if(index==candidates.size())return;
         if(target==0){
             ans.push_back(temp);
             return;
         }
-        if(index==candidates.size())return;
         
         //not pick
         combi(candidates,index+1,ans,temp,target);
